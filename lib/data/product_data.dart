be4ded1c4771a0b8ddcb8a481be6253c517bc325
LUIS_ALONSO_RIVAS_ANDRADE <a.21308051280398@cbtis128.edu.mx>
2024-04-23 18:38:57 +0000

    a

---
+++ lib/data/product_data.dart
@@ -0,0 +1,17 @@
+import 'package:rivasdisenio/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/halo.jpg",
+      title: "Halo MCC",
+      description: "Vive las experiencias de los primeros Halos solo en Xbox"),
+  ProductModel(
+      img: "assets/images/red.jpg",
+      title: "Red Dead Redemption 2",
+      description:
+          "Uno de los westerns y juegos mas emocionantes de la actualidad"),
+  ProductModel(
+      img: "assets/images/res.jpg",
+      title: "Resident Evil 4",
+      description: "Revive la experiencia del primer juego en este remake"),
+];
